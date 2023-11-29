54ffc9660296d301709fa90419d9a2cab527a822
himawaria <yui.okabe0406@gmail.com>
2023-11-29 18:06:44 +0900

    Committed

--- src/Review05.java
+++ src/Review05.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,37 +10,45 @@ import java.sql.SQLException;
 public class Review05 {
 
     public static void main(String[] args) {
+     // 3. データベース接続と結果取得のための変数宣言
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
 
         try {
+         // 1. ドライバのクラスをJava上で読み込む
             Class.forName("com.mysql.cj.jdbc.Driver");
-        
+
+         // 2. DBと接続する
             con = DriverManager.getConnection(
                     "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
                     "root",
                     "XYKFC164ma"
                 );
-            
-            String sql = "SELECT * FROM person where id = ?";
-            pstmt = con.prepareStatement(sql);
-            
+
+         // 4. DBとやりとりする窓口（PreparedStatementオブジェクト）の作成
+            String selectSql = "SELECT * FROM person where id = ?";
+            pstmt = con.prepareStatement(selectSql);
+
+         // 更新するキーワードidを入力
             System.out.print("検索キーワードを入力してください > ");
-            int input = keyIn();
-            
-            pstmt.setInt(1,input);
-            
+            int num1 = keyInNum();
+
+         // 入力されたキーワードidをPreparedStatementオブジェクトにセット
+            pstmt.setInt(1,num1);
+
+         // 5, 6. Select文の実行と結果を格納／代入
             rs = pstmt.executeQuery();
-            
+
+         // 7. 結果を表示する
             while( rs.next() ){
                 String name = rs.getString("Name");
-                int age = rs.getInt("Age");  
+                int age = rs.getInt("Age");
 
                 System.out.println(name);
-                System.out.println(age);   
+                System.out.println(age);
             }
-            
+
         } catch (ClassNotFoundException e) {
             System.err.println("JDBCドライバのロードに失敗しました。");
             e.printStackTrace();
@@ -70,17 +81,28 @@ public class Review05 {
                     e.printStackTrace();
                 }
             }
-        }   
-            
-
-    }
-        private static int keyIn() {
-            int result = 0;
-            try {
-                result = Integer.parseInt(keyIn());
-            } catch (NumberFormatException e) {
-            }
-            return result;
         }
+    }
+    private static String keyIn() {
+        String line = null;
+        try {
+            BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+            line = key.readLine();
+        } catch (IOException e) {
+
+        }
+        return line;
+    }
+
+    private static int keyInNum() {
+        int result = 0;
+        try {
+            result = Integer.parseInt(keyIn());
+        } catch (NumberFormatException e) {
+        }
+        return result;
+    }
+
+
 
 }
